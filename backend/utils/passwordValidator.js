d48b0bd9d844302c0c77355e0a907bf27fb25254
shubhamiscodding <shubham.modi.cg@gmail.com>
2026-04-04 13:11:28 +0530

    Add backend/utils/passwordValidator.js

---
+++ backend/utils/passwordValidator.js
@@ -0,0 +1,35 @@
+/**
+ * Validates password strength.
+ * Requirements:
+ *  - At least 8 characters
+ *  - At least one uppercase letter
+ *  - At least one lowercase letter
+ *  - At least one digit
+ *  - At least one special character
+ */
+const validatePassword = (password) => {
+  const errors = [];
+
+  if (!password || password.length < 8) {
+    errors.push("Password must be at least 8 characters long");
+  }
+  if (!/[A-Z]/.test(password)) {
+    errors.push("Password must contain at least one uppercase letter");
+  }
+  if (!/[a-z]/.test(password)) {
+    errors.push("Password must contain at least one lowercase letter");
+  }
+  if (!/\d/.test(password)) {
+    errors.push("Password must contain at least one number");
+  }
+  if (!/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password)) {
+    errors.push("Password must contain at least one special character");
+  }
+
+  return {
+    isValid: errors.length === 0,
+    errors,
+  };
+};
+
+module.exports = { validatePassword };
